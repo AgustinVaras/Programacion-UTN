a9119fcdf8e3147edc1500ff4a88a53f2a05c2cf
Varas Agustin <Agustin.Varas@outllook.com.ar>
2022-09-26 22:40:33 -0300

    Update Actividad 2.4.sql

--- Laboratorio 3/Clase 8/Actividad 2.4.sql
+++ Laboratorio 3/Clase 8/Actividad 2.4.sql
@@ -3,7 +3,7 @@
 Select *, DATEDIFF(day,P.FechaSolicitud, P.FechaFinalizacion) as DiasTranscurridos 
 From Pedidos P
 Where  DATEDIFF(day,P.FechaSolicitud, P.FechaFinalizacion) < (
-    Select AVG(DATEDIFF(day,P.FechaSolicitud, P.FechaFinalizacion)) as PromedioDias From Pedidos P
+    Select AVG(DATEDIFF(day,P.FechaSolicitud, P.FechaFinalizacion)*1.0) as PromedioDias From Pedidos P
 )
 
 -- 2 Los productos cuyo costo sea mayor que el costo del producto de Roble más caro.
@@ -33,21 +33,81 @@ Order By P.Costo asc
         Inner Join Productos Pr ON PR.ID = Pd.IDProducto
         Inner Join Materiales_x_Producto MxP ON MxP.IDProducto = Pd.ID
         Inner Join Materiales M ON M.ID = MxP.IDMaterial
-        Where (M.Nombre <> 'Pino') and (Year(Pd.FechaSolicitud) = 2022)
+        Where (M.Nombre = 'Pino') and (Year(Pd.FechaSolicitud) = 2022)
     )
 
-    Select COUNT(*) From Pedidos Pd
-    Inner Join Productos Pr ON PR.ID = Pd.IDProducto
-    Inner Join Materiales_x_Producto MxP ON MxP.IDProducto = Pd.ID
-    Inner Join Materiales M ON M.ID = MxP.IDMaterial
-    Where M.Nombre <> 'Pino'
-
+    Select * From (
+        Select Cli.Nombres, Cli.Apellidos, Cli.Mail 
+    From Clientes Cli 
+    Where Cli.ID not in(
+        Select distinct Pd.IDCliente From Pedidos Pd
+        Inner Join Productos Pr ON PR.ID = Pd.IDProducto
+        Inner Join Materiales_x_Producto MxP ON MxP.IDProducto = Pr.ID
+        Inner Join Materiales M ON M.ID = MxP.IDMaterial
+        Where M.Nombre = 'Pino' and Year(Pd.FechaSolicitud) = 2022
+        )
+     ) PedidosNoPino2022
 
 -- 4 Los colaboradores que no hayan realizado ninguna tarea de Lijado en pedidos que se solicitaron en el año 2021.
+Select distinct C.Legajo, C.Nombres, C.Apellidos, C.Sueldo, P.FechaSolicitud AS FechadePedido, T.Nombre as TipoTarea
+ From Colaboradores C
+Inner Join Tareas_x_Pedido TxP ON TxP.Legajo = C.Legajo
+Inner Join Pedidos P ON TxP.IDPedido = P.ID
+Inner Join Tareas T ON T.ID = TxP.IDTarea
+Where T.Nombre <> 'Lijado' and YEAR(P.FechaSolicitud) = '2021' 
+
+Select * From(
+    Select distinct C.Legajo, C.Nombres, C.Apellidos, C.Sueldo 
+ From Colaboradores C
+ Where C.Legajo not in(
+     Select TxP.Legajo From Tareas_x_Pedido TxP
+    Inner Join Pedidos P ON TxP.IDPedido = P.ID
+    Inner Join Tareas T ON T.ID = TxP.IDTarea
+    Where T.Nombre = 'Lijado' and YEAR(P.FechaSolicitud) = '2021' 
+    )
+ ) as NoLijados2021
 
 -- 5 Los clientes a los que les hayan enviado (no necesariamente entregado) al menos un tercio de sus pedidos.
 
+Select * From (Select Cli.Nombres Nombres, Cli.Apellidos Apellidos,
+    (
+    Select Count(*) From Pedidos P
+    Where P.IDCliente = Cli.ID
+    ) as PedidosTotales,
+    (
+        Select Count(*) From Pedidos P 
+        Inner Join Envios E ON E.IDPedido = P.ID
+        Where P.IDCliente = Cli.ID
+    ) as CantEnvios
+From Clientes Cli ) as Envios_x_Cliente
+Where CantEnvios >= PedidosTotales/3.0 
+
+
 -- 6 Los colaboradores que hayan realizado todas las tareas (no necesariamente en un mismo pedido).
+
+Select * From (
+    Select Distinct Count(*) as Total From Tareas 
+) TotalTareas
+
+Select * From (
+    Select *, (
+        Select Distinct Count(*) From Tareas T
+        Inner Join Tareas_x_Pedido TxP ON TxP.IDTarea = T.ID
+        Where TxP.Legajo = C.Legajo
+    ) as TareasxColaborador,
+    (
+        Select Distinct Count(*) as Total From Tareas
+    ) as TotalTareas
+    From Colaboradores C
+    ) as Lista 
+    Where Lista.TareasxColaborador = Lista.TotalTareas
+
+Select C.Nombres, (
+        Select DISTINCT Count(*) From Tareas T
+        Left Join Tareas_x_Pedido TxP ON TxP.IDTarea = T.ID
+        Where TxP.Legajo = C.Legajo
+    ) as TareasxColaborador
+    From Colaboradores C
 
 -- 7 Por cada producto, la descripción y la cantidad de colaboradores fulltime que hayan trabajado en él y la cantidad de colaboradores parttime.
 
@@ -56,6 +116,8 @@ Order By P.Costo asc
 -- 9 Por cada cliente, apellidos y nombres y la cantidad de pedidos solicitados en los años 2020, 2021 y 2022. (Cada año debe mostrarse en una columna separada)
 
 -- 10 Por cada producto, listar la descripción del producto, el costo y los materiales de construcción (en una celda separados por coma)
+
+
 
 -- 11 Por cada pedido, listar el ID, la fecha de solicitud, el nombre del producto, los apellidos y nombres de los colaboradores que trabajaron en el pedido y 
 -- la/s tareas que el colaborador haya realizado (en una celda separados por coma)
